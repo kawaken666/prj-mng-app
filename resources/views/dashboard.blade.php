32ad9eaebd815ef3364b8c3b8199e2b33480af6e
kawaken666 <kawaken666.2@gmail.com>
2024-02-25 23:56:05 +0900

    ダッシュボードのUIを綺麗にした

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,26 +7,34 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6">
-                    <table class="table-auto text-center border-2">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
+                <table class="mx-auto text-sm w-11/12 border">
+                    <thead class=" bg-fuchsia-400">
                         <tr>
-                            <th class="border-2" bgcolor="violet">プロジェクト名</th>
-                            <th class="border-2" bgcolor="violet">見積もり</th>
-                            <th class="border-2" bgcolor="violet">リリース予定日</th>
-                            <th class="border-2" bgcolor="violet">稼働予定月</th>
+                            <th class="px-6 py-3">プロジェクト名</th>
+                            <th class="px-6 py-3">見積もり</th>
+                            <th class="px-6 py-3">リリース予定日</th>
+                            <th class="px-6 py-3">稼働予定月</th>
                         </tr>
-                        @foreach($projects as $project)
-                            <tr>
-                                <td class="border-2 underline text-blue-500"><a href="{{ route('project.detail', ['project_id' => $project->project_id]) }}">{{ $project->project_name }}</a></td>
-                                <td class="border-2">{{ $project->estimation }}</td>
-                                <td class="border-2">{{ $project->release_date }}</td>
-                                <td class="border-2">{{ $project->work_date }}</td>
-                            </tr>
-                        @endforeach
-                    </table>
-                </div>
+                    </thead>
+                    <tbody>
+                    @foreach($projects as $project)
+                        <tr class="text-center">
+                            <td class="px-6 py-3 underline text-blue-500"><a href="{{ route('project.detail', ['project_id' => $project->project_id]) }}">{{ $project->project_name }}</a></td>
+                            <td class="px-6 py-3">{{ $project->estimation }}</td>
+                            <td class="px-6 py-3">{{ $project->release_date }}</td>
+                            <td class="px-6 py-3">{{ $project->work_date }}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+
+
+    <div class="relative overflow-x-auto">
+
+    </div>
+
 </x-app-layout>
